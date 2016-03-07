19378721aa6556fab1190c2265556a914eac2c5c
pinkslot <pinkslot@github.com>
2016-03-08 08:39:02 +1000

    fix calc_stat

--- Task.cpp
+++ Task.cpp
@@ -62,25 +62,47 @@ void Task::point_go() {
 	cout << "DONE " << (clock() - start) / (double)CLOCKS_PER_SEC << endl;
 }
 
+double ans(double t) {
+	return exp(-t / 10.);
+}
+double exp_J(sp<Point> x) {
+	return (x->media->mu - x->media->mu_s - 1. / 10. / x->media->v) * ans(x->t);
+}
+ofstream log_out;
+void print_to_streams(vector<double> v) {
+	for (auto &i : v) {
+		cout << i << '\t';
+		log_out << i << '\t';
+	}
+	cout << endl; log_out << endl;
+}
+
 void Task::calc_stat() {
 	cout << "start on " << task_name << endl;
+	char log_fname[20];
+	sprintf_s(log_fname, "%s.log", task_name.c_str());
+	log_out = ofstream(log_fname);
+	(cout << std::scientific).precision(3);
+	(log_out << std::scientific).precision(3);
+	string header("n\t f\t\t eps\t\t delf\t\t r_delf\t\t complex\n");
+	cout << header;
+	log_out << header;
 
-	int TIME = 10;
-	for (double n = 2; n < 100; n++) {
-		Point * p = new Point(TIME, make_array3d(0., 0., 5), make_array3d(0, 0, 1), G);
+	double TIME = 10;
+	for (int n = 2; n < 100; n++) {
+		Point * p = new Point(TIME, make_array3d(1., 0., z_screen), make_array3d(0, 0, 1), G);
 		clock_t start = clock();
-		double f = 0, df = 0;
+		double Ef = 0, Ef2 = 0;
 		for (int i = 0; i < k; i++) {
-			double ff = p->f(n);
-			f += ff;
-			df += ff * ff;
+			double f = p->f(n);
+			Ef = double(i) / (i + 1) * Ef + f / (i + 1);
+			Ef2 = double(i) / (i + 1) * Ef2 + f * f / (i + 1);
 			if (i % 1000 == 0) cout << '\xd' << i / 1000 << '/' << k / 1000;
 		}
 		double avg_time = (clock() - start) / (double)CLOCKS_PER_SEC / k;
-		f /= k;
-		df = sqrt((df - f * f * k) / (k - 1)) / f;
-
-		cout << '\xd' <<  "f = " << f << " \teps=" << f / exp(TIME) - 1  << "\tdelf(" << n << ")= " << df << "\tcomplexity = " << df * avg_time<< endl;
+		double Df = double(k) / (k - 1) *(Ef * Ef - Ef2);
+		cout << '\xd' << n << '\t'; log_out << n << '\t';
+		print_to_streams({Ef, Ef / ans(TIME) - 1, Df , Df / Ef, Df * avg_time});
 	}
 }
 void Task1::init() {
@@ -113,14 +135,16 @@ void Task2::init() {
 }
 
 void Task3::init() {
-	G = new SphereMedia(1., .01, .01, 1., 10., make_array3d(2, 0, 0), []() { return 1.; });
-	GlobalMedia::instance( [](sp<BorderPoint> x) { return 1.; })->add_submedia(G);
-	G->add_submedia(new SphereMedia(1., .5, .5, 4., 1., make_array3d(0, 0, 0), []() { return 1.; }));
+	G = new SphereMedia(4, 2, 1., 10., make_array3d(0, 0, 0), indic_direct, exp_J);
+	GlobalMedia::instance([](sp<BorderPoint> x) { return ans(x->t); }, 1)->add_submedia(G);
+	G->add_submedia(new SphereMedia(2, 1, 4., 4., make_array3d(0, 0, 0), indic_direct, exp_J));
+
 	res = 200, color_mul = 150;
-	n = 15, m = 1, k = 1;
-	size = 5., time_step = .5, min_time = 10, max_time = 30, z_screen = 3,
+	n = 5, k = 1e4;
+	size = 5., time_step = 1, min_time = 2, max_time = 50, z_screen = 4,
 		size2 = size / 2, step = size / res;
-	task_name = string("trd");
+	task_name = string("fifth");
+	calc_stat();
 }
 
 
